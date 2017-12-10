d192058620db77f715958fd8a8caec18cf370c8e
legomir <pawelbernaciak@outlook.com>
2017-12-10 13:39:39 +0100

    add save_composed method

--- overlay.py
+++ overlay.py
@@ -42,14 +42,21 @@ class Overlay(object):
         """
         self.out_img.save(filename)
 
+    def save_composed(self, filename):
+        source_rgba = Image.new(size=self.source_img.size, mode='RGBA')
+        source_rgba.paste(self.source_img)
+        out = Image.alpha_composite(source_rgba, self.out_img)
+
+        out.save(filename)
+
     def draw_block(self, text, drawing_point):
         draw = ImageDraw.Draw(self.out_img)
 
-        text_bbox_x, text_bbox_y = draw.textsize(
+        text_size_x, text_size_y = draw.textsize(
             text, font=self.fonts['inconsolata_regular'])
 
-        fill_size_x = int(text_bbox_x *  self.text_fill_scale)
-        fill_size_y = int(text_bbox_y *  self.text_fill_scale)
+        fill_size_x = int(text_size_x *  self.text_fill_scale)
+        fill_size_y = int(text_size_y *  self.text_fill_scale)
 
         x0, y0 = self._drawing_points(
             drawing_point,
@@ -58,7 +65,7 @@ class Overlay(object):
 
         fill_opacity = int(self.text_fill_opacity * 255)
         draw.rectangle(
-            [x0, y0, fill_size_x, fill_size_y],
+            [x0, y0, x0 + fill_size_x, y0 + fill_size_y],
             fill=(0, 0, 0, fill_opacity)
         )
 
@@ -107,8 +114,8 @@ class Overlay(object):
 
         :returns: (start_point_x, start_point_y)
         """
-        pad_height = int(self.height * self.text_fill_scale)
-        pad_width = int(self.width * self.text_fill_scale)
+        pad_height = int(self.height * self.text_padding)
+        pad_width = int(self.width * self.text_padding)
         bbox_x, bbox_y = text_bbox
         center = int(self.width / 2)
 
@@ -117,7 +124,7 @@ class Overlay(object):
         center_start = center - int(bbox_x / 2)
 
         corners = {
-            'up_left': (pad_width, pad_height),
+            'up_left': (pad_height, pad_height),
             'up_center': (center_start, pad_height),
             'up_right': (right_start, pad_height),
             'bottom_left': (pad_width, bottom_y),
@@ -165,5 +172,5 @@ def scale_bbox(bbox, percent):
 if __name__ == '__main__':
     imgpath = 'test_image.jpg'
     img = Overlay(imgpath)
-    img.draw_block('some stupid text', 'up_left')
-    img.save('test.png')
+    img.draw_block('some stupid text', 'up_right')
+    img.save_composed('test.png')
